aab8e974d1275a9577cd8f51c496ff2d392f03ac
Dobrynya <antonov2766@mail.ru>
2023-10-23 15:04:23 +0300

    добавил плеер от реакта

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import './Footer.css';
+
+function Footer() {
+
+    return (
+        <footer className="footer">
+            <h1>FOOTER</h1>
+
+        </footer>
+    );
+}
+
+export default Footer;
--- src/Main.js
+++ src/Main.js
@@ -1,3 +1,5 @@
+import React from 'react'
+import ReactPlayer from 'react-player/youtube'
 import Header from './Header';
 import image from './image/IMG_2477-min.png';
 import image2 from './image/imgonline-com-ua-Blur-oA2GPjnu1b-min.jpg';
@@ -6,11 +8,18 @@ import './main.css';
 
 function Main() {
     return (
-        <div className="Main">
-               <Header />
+        <div className="main">
+            <Header />
             <img src={image} className="main__image" alt="фото музыкальной группы Fake band" />
-            <img src={image2} className="main__image2" alt="кавер группа fake band" />
-         
+            <div className='main__image-container'>
+                <img src={image2} className="main__image2" alt="кавер группа fake band" />
+                <p className='main__text'>МОЛОДОСТЬ!</p>
+                <p className='main__text2'>СТИЛЬ!</p>
+                <p className='main__text3'>ДРАЙВ!</p>
+                <div className='main__iframe'>
+                    < ReactPlayer url='https://www.youtube.com/watch?v=76kOHCdbBrU' controls={true} width="100%" height="100%" />
+                </div>
+            </div>
         </div>
     );
 }
