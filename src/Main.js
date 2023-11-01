e754fec2bc709d06f277f610643fa83e5b8a276a
Dobrynya <antonov2766@mail.ru>
2023-11-01 15:16:26 +0300

    финал

--- src/Main.js
+++ src/Main.js
@@ -7,8 +7,10 @@ import exampleImage6 from './image/Property 1=Frame 6.png';
 import exampleImage7 from './image/Property 1=Frame 7.png';
 import exampleImage8 from './image/Property 1=Frame 8.png';
 import exampleImage9 from './image/Property 1=Frame 9.png';
+import video from './image/FakeBand CLIP v3 (1).mp4';
 
 import './main.css';
+import ReactPlayer from 'react-player';
 
 
 function Main() {
@@ -25,13 +27,45 @@ function Main() {
         return () => clearTimeout(interval);
     }, [currentImageIndex, images.length]);
 
+
+    const [showVideo, setShowVideo] = useState(false);
+
+    const handleMouseOver = () => {
+        setShowVideo(true);
+    };
+
+    const handleMouseOut = () => {
+        setShowVideo(false);
+    };
     return (
         <section className="main">
             <h1 className='main__text'>FAKE BAND</h1>
             <p className='main__par'>OFFICIAL SITE</p>
             <div className='main__promo-container'>
-                <a href='https://www.youtube.com/watch?v=76kOHCdbBrU' className='main__promo' target="_blank" rel="noreferrer">PROMO</a>
-                <img className='main__promo-frame' src={images[currentImageIndex]} alt='' />
+                <a
+                    href='https://www.youtube.com/watch?v=76kOHCdbBrU'
+                    className='main__promo'
+                    target="_blank"
+                    rel="noreferrer"
+                    onMouseOver={handleMouseOver}
+                    onMouseOut={handleMouseOut}>PROMO
+
+                </a>
+                <img className='main__promo-frame' src={images[currentImageIndex]} alt='компоненты для визуализации' />
+                <div className={`main__promo-video ${showVideo ? 'show' : ''}`}>
+                    <ReactPlayer
+
+                        url={video}
+                        playing={true}
+                        loop={true}
+                        muted={true}
+                        controls={false}
+                        width= '440'
+                        height= '248'
+                        playsinline
+                    ></ReactPlayer>
+                </div>
+
             </div>
 
             <ul className='main__socials'>
@@ -41,7 +75,7 @@ function Main() {
             </ul>
 
             <ul className='main__contact'>
-                <li className='main__socials-link'> <a className='main__contact-tel'  href='tel:+79779329001' target="_blank" rel="noreferrer">+7 977 932-90-01</a></li>
+                <li className='main__socials-link'> <a className='main__contact-tel' href='tel:+79779329001' target="_blank" rel="noreferrer">+7 977 932-90-01</a></li>
                 <li className='main__socials-link'><a className='main__contact-lin-text' href='https://wa.me/79834065617' target="_blank" rel="noreferrer">WHATSAPP</a></li>
                 <li className='main__socials-link'><a className='main__contact-lin-text' href='https://t.me/Timurchikss' target="_blank" rel="noreferrer">TELEGRAM</a></li>
             </ul>
